182f420a1ff20e959f9694233bcbfebb8c1a8cfc
Anirudh Madhavan <anirudh@beatworx.in>
2018-12-24 17:24:59 +0530

    add : brand logos

--- js/renderer.js
+++ js/renderer.js
@@ -1,37 +1,71 @@
 (function() {
 	const partners = [
 		{
-			imageUrl: "https://placehold.it/200x200",
+			imageUrl: "images/brand-logos/akg-logo.svg",
 			link: ""
 		},
 		{
-			imageUrl: "https://placehold.it/200x200",
+			imageUrl: "images/brand-logos/api.png",
 			link: ""
 		},
 		{
-			imageUrl: "https://placehold.it/200x200",
+			imageUrl: "images/brand-logos/audio-technica.png",
 			link: ""
 		},
 		{
-			imageUrl: "https://placehold.it/200x200",
+			imageUrl: "images/brand-logos/heritage-audio.png",
 			link: ""
 		},
 		{
-			imageUrl: "https://placehold.it/200x200",
+			imageUrl: "images/brand-logos/lewitt.png",
 			link: ""
 		},
 		{
-			imageUrl: "https://placehold.it/200x200",
+			imageUrl: "images/brand-logos/manley.png",
+			link: ""
+		},
+		{
+			imageUrl: "images/brand-logos/pmc.png",
+			link: ""
+		},
+		{
+			imageUrl: "images/brand-logos/royer.png",
+			link: ""
+		},
+		{
+			imageUrl: "images/brand-logos/rupert-neive.png",
+			link: ""
+		},
+		{
+			imageUrl: "images/brand-logos/ssl.png",
+			link: ""
+		},
+		{
+			imageUrl: "images/brand-logos/ua.png",
+			link: ""
+		},
+		{
+			imageUrl: "images/brand-logos/warm-audio.png",
 			link: ""
 		}
 	];
 
 	let partnersRenderer = function() {
 		let html = "";
+
 		partners.map(partner => {
+			let brand = String(partner.imageUrl)
+				.split("/")[2]
+				.split(".")[0];
+			let classString = "";
+			if (brand === "pmc") {
+				classString = "pmc";
+			} else if (brand === "royer") {
+				classString = "royer";
+			}
 			html += `<li class="margin-ten-bottom sm-margin-six-bottom xs-margin-10px-bottom">
 				<a href="${partner.link}" target="_blank">
-					<img src="${partner.imageUrl}">
+					<img class="brand-logo-image ${classString}" src="${partner.imageUrl}">
 				</a>
 			</li>`;
 		});
